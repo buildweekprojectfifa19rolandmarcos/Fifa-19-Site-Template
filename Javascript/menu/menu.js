747c6a1d3c7b4cb463ae7e8fc6e3b7ceb15a198a
Roland J Jarvis <Rjarvis87@gmail.com>
2019-06-28 01:04:48 -0400

    Menu now has animations and its HNNNNGH good!

--- Javascript/menu/menu.js
+++ Javascript/menu/menu.js
@@ -21,6 +21,7 @@ const toggleMenu = () => {
   const menu = document.querySelectorAll('.menu')[0];
   // create a reference to the ".menu-button" class
   const menuButton = document.querySelectorAll('.menu-button');
+  console.log(menuButton)
   // Using your menuButton reference, add a click handler that calls toggleMenu
   menuButton[0].addEventListener('click', (element)=>{ toggleMenu() });
   
