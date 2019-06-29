623545c48a1d57c4f6a0a25f77eab2eddd778cae
Roland J Jarvis <Rjarvis87@gmail.com>
2019-06-29 12:59:49 -0400

    border changes

--- Javascript/menu/menu.js
+++ Javascript/menu/menu.js
@@ -4,19 +4,6 @@ const toggleMenu = () => {
     menu.classList.toggle('menu--open');
   }
   
-  /*
-  class Button{
-    constructor(element){
-      this.element = element;
-    }
-    
-    click(){
-        
-        this.element.addEventListener('click', () => { this.toggleMenu() });
-    }
-  }
-  */
-  
   // Start Here: Create a reference to the ".menu" class
   const menu = document.querySelectorAll('.menu')[0];
   // create a reference to the ".menu-button" class
