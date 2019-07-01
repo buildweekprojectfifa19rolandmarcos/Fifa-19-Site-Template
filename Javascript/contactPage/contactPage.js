f2a6a14892553692a86f93b48856034564ef6835
Marcos Cabrera <ubermoc@pop-os.localdomain>
2019-07-01 05:22:59 -0700

    added js to the contact page

--- Javascript/contactPage/contactPage.js
+++ Javascript/contactPage/contactPage.js
@@ -1,3 +1,9 @@
-function SEND() {
-    document.getElementById("form").SEND();
+document.getElementById("form1").onsubmit=function() {
+    first = document.getElementById("firstname").value;
+    last = document.getElementById("lastname").value;
+    eMail= document.getElementById("eMail").value;
+    message = document.getElementById("message").value;
+    yourName = first + " " + last;
+    document.getElementById("yourname").innerHTML = "Hi " + yourName + ". Your message " + message + " has been submitted.From " + eMail + ".";
+    return false;
   }
